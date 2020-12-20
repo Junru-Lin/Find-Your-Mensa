a2fa0783a89668fcfc83de371c28d454f55b8067
Junru-Lin <junrulinaurora@gmail.com>
2020-12-20 16:35:50 +0100

    adjust layout of help page, list page, favourite page and filter page.
    make the bottomNavigation in help page work.
    
    make the help botton in each page work.

--- app/src/main/java/com/junru/findyourmensa/HelpActivity.java
+++ app/src/main/java/com/junru/findyourmensa/HelpActivity.java
@@ -1,10 +1,17 @@
 package com.junru.findyourmensa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 // By Junru Lin
 public class HelpActivity extends AppCompatActivity {
@@ -15,5 +22,30 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
 
 
+        //Initialize and assign variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.page_1:
+                        startActivity(new Intent(getApplicationContext(), ListActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.page_2:
+                        startActivity(new Intent(getApplicationContext(), FilterActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.page_3:
+                        startActivity(new Intent(getApplicationContext(), FavouritesActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+
 }
