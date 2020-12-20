660077389aa1f47cfcb853f2c0002f377156fb9a
kartographinya <kalerya1996@gmail.com>
2020-12-20 14:15:39 +0100

    Added help button to the ListActivity.

--- app/src/main/java/com/junru/findyourmensa/ListActivity.java
+++ app/src/main/java/com/junru/findyourmensa/ListActivity.java
@@ -2,16 +2,33 @@ package com.junru.findyourmensa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        button = findViewById(R.id.help_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openHelpActivity();
+            }
+        });
     }
 
-
+    private void openHelpActivity() {
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
 }
+
