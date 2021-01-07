df96d09b7b0d51a1898899652bd7487a3d30086d
kartographinya <kalerya1996@gmail.com>
2021-01-07 11:20:28 +0100

    DayPlanActivity created

---
+++ app/src/main/java/com/junru/findyourmensa/DayPlanActivity.java
@@ -0,0 +1,14 @@
+package com.junru.findyourmensa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_plan);
+    }
+}
